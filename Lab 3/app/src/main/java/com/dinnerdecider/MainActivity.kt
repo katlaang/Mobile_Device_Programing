c8f07d5c42c56854839174d4a74ec8641e7afafd
Eunice Myllynen <eunice.kamau@hotmail.com>
2022-02-20 14:51:54 -0600

    Adding more files

--- Lab 3/app/src/main/java/com/dinnerdecider/MainActivity.kt
+++ Lab 3/app/src/main/java/com/dinnerdecider/MainActivity.kt
@@ -2,10 +2,28 @@ package com.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.random.Random as Random1
 
 class MainActivity : AppCompatActivity() {
+    val dinnerList= arrayListOf( "Hamburger", "Pizza", "Mexican", "American", "Chinese")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    //clicklistener for decide button to select random food
+        decideBtn.setOnClickListener{
+            val random= Random()
+            val selectedRandomFood=random.nextInt(dinnerList.size)
+            randomSelectedFood.text=dinnerList[selectedRandomFood]
+
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+        }
     }
+
 }
