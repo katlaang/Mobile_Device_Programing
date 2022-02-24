b465e770b5d4db142fa036f74437fc5e03131bab
Eunice Myllynen <eunice.kamau@hotmail.com>
2022-02-23 22:22:38 -0600

    changes to mainActivity

--- Lab4/TableLayoutTest/app/src/main/java/com/tablelayouttest/MainActivity.kt
+++ Lab4/TableLayoutTest/app/src/main/java/com/tablelayouttest/MainActivity.kt
@@ -2,10 +2,42 @@ package com.tablelayouttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TableRow
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+
+    fun init() {
+    if(vTxt.text.isEmpty() || cTxt.text.isEmpty()){
+        if (vTxt.text.isEmpty()){
+            vTxt.requestFocus()
+        }
+        else{
+            cTxt.requestFocus()
+        }
+    }else{
+        Toast.makeText(applicationContext, "All fields are required", Toast.LENGTH_LONG).show()
+    }
+
+        //make table rows
+        val tableRow =TableRow(applicationContext)
+        val layoutParams=TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+        layoutParams.setMargins(0,20,0,0)
+        layoutParams.gravity= Gravity.BOTTOM
+        tableRow.layoutParams=layoutParams
+
+
+    val version=vTxt.text.toString().trim()
+        val code=cTxt.text.toString().trim()
     }
 }
+
+
