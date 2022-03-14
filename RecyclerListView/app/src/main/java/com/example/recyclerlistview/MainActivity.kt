7188817fa60b1041c7402cb4f99336527985c8e3
Eunice Myllynen <eunice.kamau@hotmail.com>
2022-03-13 20:53:09 -0500

    recycling update

--- RecyclerListView/app/src/main/java/com/example/recyclerlistview/MainActivity.kt
+++ RecyclerListView/app/src/main/java/com/example/recyclerlistview/MainActivity.kt
@@ -9,10 +9,10 @@ class MainActivity : AppCompatActivity() {
     var r1: RecyclerView? = null
     var layoutManager: RecyclerView.LayoutManager? = null
     var productList = ArrayList<Product>(arrayListOf(
-        Product("Tablet",480.10,"Black",R.drawable.tablet.toString(),"101","The Tab S7 Plus is Samsung’s flagship model and offers all the bells and whistles you could want in a tablet. The giant, crisp screen is perfect for watching TV shows or doing serious work on things like spreadsheets or word processing documents."),
-        Product("Printer",343.10,"Black",R.drawable.printer.toString(),"102","Need excellent-quality borderless photos at sizes up to 8.5 by 11 inches? Epson's six-ink Expression Photo XP-8700 will get you those sharp prints, plus do some light-duty document handling"),
-        Product("Laptop",450.10,"Silver",R.drawable.laptop.toString(),"104","A laptop, often called a notebook, is a small, portable personal computer with a \"clamshell\" form factor, typically having a thin LCD or LED computer screen mounted on the inside of the upper lid of the clamshell and an alphanumeric keyboard on the inside of the lower lid."),
-        Product("TeleVision",600.75,"Silver",R.drawable.television.toString(),"105","A television (also known as a TV) is a machine with a screen. Televisions receive broadcasting signals and turn them into pictures and sound. The word \"television\" comes from the words tele (Greek for far away) and vision (sight). Sometimes a television can look like a box.")
+        Product("Samsung S7",480.10,"Black",R.drawable.tablet.toString(),"101","The Tab S7 Plus is Samsung’s flagship model and offers all the bells and whistles you could want in a tablet. The giant, crisp screen is perfect for watching TV shows or doing serious work on things like spreadsheets or word processing documents."),
+        Product("Epson XP-8700 ",343.10,"Black",R.drawable.printer.toString(),"102","Need excellent-quality borderless photos at sizes up to 8.5 by 11 inches? Epson's six-ink Expression Photo XP-8700 will get you those sharp prints, plus do some light-duty document handling"),
+        Product("Lenovo Slim 7",450.10,"Silver",R.drawable.laptop.toString(),"104","Putting Pro in the name of a laptop model means different things to different people. In this case, it seems that it means moving a notch up from the regular IdeaPad Slim 7 in almost every way. The Slim 7i Pro (the letter i is for the Intel processor) has better components, a higher-quality display and some future-proofing tech, as well as some features to improve your work experience."),
+        Product("Samsung QLED",600.75,"Silver",R.drawable.television.toString(),"105","The Neo QN900 QLED TV is a stunning TV that sets a new benchmark when it comes to high-end televisions. It's significantly brighter and shrinks down the bezel to almost nothing.")
     ))
     var myAdapter : MyAdapter?=null
 
