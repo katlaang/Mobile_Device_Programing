4e88b1c0e5f827285e7e0cec199a0d2631a1ab40
Eunice Myllynen <eunice.kamau@hotmail.com>
2022-04-09 12:32:01 -0500

    options menu

--- CV_App/app/src/main/java/com/app/cv/MainActivity.kt
+++ CV_App/app/src/main/java/com/app/cv/MainActivity.kt
@@ -2,10 +2,42 @@ package com.app.cv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.DrawableRes
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupViewPager(viewPager)
+        setUpIcons()
+
+
+    }
+
+
+    private fun setupViewPager(viewPager: ViewPager){ //private method to set up the swipe views
+        val  viewPagerAdapter=ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.addFragment(HomeFragment(), "Home")
+        viewPagerAdapter.addFragment(Education(), "Education")
+        viewPagerAdapter.addFragment(Skills(), "Skills")
+        viewPagerAdapter.addFragment(Work(), "Work")
+        viewPagerAdapter.addFragment(Hobbies(), "Hobbies")
+        viewPagerAdapter.addFragment(References(), "References")
+        viewPagerAdapter.addFragment(Contact(), "Contact")
+
+        viewPager.adapter=viewPagerAdapter
+    }
+
+    private fun setUpIcons(){
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.home)
+
     }
 }
--- CV_App/app/src/main/java/com/app/cv/ViewPagerAdapter.kt
+++ CV_App/app/src/main/java/com/app/cv/ViewPagerAdapter.kt
@@ -1,4 +1,29 @@
 package com.app.cv
 
-class ViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+@Suppress("DEPRECATION")
+class ViewPagerAdapter(fm:FragmentManager): FragmentPagerAdapter(fm) {//used for swipable views
+    private val fragments= mutableListOf<Fragment>()
+    private val fragmentTitle= mutableListOf<String>()
+
+    fun addFragment(fragment: Fragment, title:String){
+        fragments.add(fragment)
+        fragmentTitle.add(title)
+    }
+
+    override fun getCount(): Int {
+        return fragments.size
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return fragmentTitle[position]
+    }
 }
