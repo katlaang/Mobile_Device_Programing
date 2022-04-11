ee234a4bbab609a1dc64cff6c267467102e74aa9
Eunice Myllynen <eunice.kamau@hotmail.com>
2022-04-11 01:08:30 -0500

    phone dial fragment

--- CV_App/app/src/main/java/com/app/cv/ReferencesFragment.kt
+++ CV_App/app/src/main/java/com/app/cv/ReferencesFragment.kt
@@ -1,21 +1,23 @@
+@file:Suppress("DEPRECATION")
+
 package com.app.cv
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_references.*
+import java.util.jar.Manifest
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [References.newInstance] factory method to
- * create an instance of this fragment.
- */
 class References : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,12 +25,51 @@ class References : Fragment() {
         setHasOptionsMenu(true)
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_references, container, false)
-    }
+//        val intent = Intent(Intent.ACTION_DIAL)
+//        intent.data = Uri.parse("tel:123 456789")
+//        startActivity(intent)
 
+        val view: View = inflater.inflate(R.layout.fragment_references, container, false)
+        return view
+    }
+    
 }
+
+//    private fun call() {
+//        if (ContextCompat.checkSelfPermission(
+//                requireContext(),
+//                android.Manifest.permission.CALL_PHONE
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            val callIntent = Intent(Intent.ACTION_CALL)
+//            callIntent.data = Uri.parse("tel:" + "123")
+//            requireActivity().startActivity(callIntent)
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                requireActivity(), arrayOf(android.Manifest.permission.CALL_PHONE),
+//                References.CALL_PERMISSION_REQUEST_CODE
+//            )
+//        }
+//    }
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == CALL_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    companion object {
+//        private const val CALL_PERMISSION_REQUEST_CODE = 1234
+//    }
+
+
