1437418904abbbe8d495076a09892a4903b4bcb5
Eunice Myllynen <eunice.kamau@hotmail.com>
2022-04-11 19:56:01 +0300

    call button on contact fragment

--- CV_App/app/src/main/java/com/app/cv/ContactFragment.kt
+++ CV_App/app/src/main/java/com/app/cv/ContactFragment.kt
@@ -1,12 +1,17 @@
 package com.app.cv
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_contact.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -40,6 +45,9 @@ class Contact : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_contact, container, false)
 
+        val btnFragments: Button = view.findViewById(R.id.btn_call_home) as Button
+        btnFragments.setOnClickListener { call() }
+
         view.website.setOnClickListener {
             val url = "https://fi.linkedin.com/in/teemu-myllynen-b0a14283"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
@@ -48,8 +56,30 @@ class Contact : Fragment() {
         return view
     }
 
+    fun call() {
+        val num = "+16418191588"
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CALL_PHONE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse("tel:" + "+16418191588")
+            activity?.startActivity(callIntent)
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.CALL_PHONE),
+                CALL_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
        inflater.inflate(R.menu.menu_main, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    companion object {
+        private const val CALL_PERMISSION_REQUEST_CODE = 1234
     }
 }
